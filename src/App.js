4d1ff3d0a7009b83577b3e4f4f5f0c8bc2e7c794
VasylDrebot <drebotvasyl19960101@gmail.com>
2022-01-27 21:08:18 +0200

    homework4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import {useReducer} from 'react';
+
 import './App.css';
 
+const reducer = (state, action) => {
+        switch (action.type) {
+            case 'cat':
+                return {...state, cat:state.cat}
+        }
+}
+
 const App = () => {
-  return (
-      <div>
-        <h1>Hello!</h1>
-      </div>
-  );
+    const [state, dispatch] = useReducer(reducer, {cat: '', dog: ''})
+    const formHandler = (e) => {
+        const data = {...state, [e.target.name]: e.target.valueO}
+    }
+    return (
+        <div>
+            <form>
+                <label>Add cat:
+                    <input type="text" name={'cat'} value={state.cat} onChange={formHandler}/>
+                    <button onClick={()=>dispatch({type:'cat'})}>Save</button>
+                </label>
+                <label>Add dog:
+                    <input type="text" name={'dog'} value={state.dog} onChange={formHandler}/>
+                    <button onClick={()=>dispatch({type:'dog'})}>Save</button>
+                </label>
+            </form>
+            <hr/>
+        </div>
+    );
 };
 
 export default App;
