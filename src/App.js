dd40c5d7e7cbf0ad387b055e71e5c2b076c32c73
VasylDrebot <drebotvasyl19960101@gmail.com>
2022-01-27 16:52:33 +0200

    homework4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import {useReducer} from 'react';
+
 import './App.css';
 
+const reducer = (state, action) => {
+    state = {...state, count2: state.count2 + 1}
+    switch (action.type) {
+        case 'inc':
+            return {...state, count1: state.count1 + 1}
+        case 'dec':
+            return {...state, count1: state.count1 - 1}
+        case 'reset':
+            return {...state, count1: action.payload}
+        default:
+            throw new Error('MyError')
+    }
+}
+
 const App = () => {
+    const [state, dispatch] = useReducer(reducer, {count1:0, count2:0});
   return (
       <div>
-        <h1>Hello!</h1>
+          <div>{state.count1}</div>
+          <div>{state.count2}</div>
+          <button onClick={()=>dispatch({type:'inc'})}>INC</button>
+          <button onClick={()=>dispatch({type:'dec'})}>DEC</button>
+          <button onClick={()=>dispatch({type:'reset', payload: 0})}>RESET</button>
       </div>
   );
 };
