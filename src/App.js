255162af38bf096b2856324c362143c5c52d2f92
VasylDrebot <drebotvasyl19960101@gmail.com>
2022-01-29 14:16:07 +0200

    homework4

--- src/App.js
+++ src/App.js
@@ -2,28 +2,64 @@ import {useReducer} from 'react';
 
 import './App.css';
 
+const initialState = {
+    cat: '',
+    dog: '',
+};
+
 const reducer = (state, action) => {
-        switch (action.type) {
-            case 'cat':
-                return {...state, cat:state.cat}
-        }
+    switch (action.type) {
+        case 'update_input':
+            let res = {...state,
+            [action.key]: action.value}
+            return res;
+        default:
+            return state;
+    }
 }
 
 const App = () => {
-    const [state, dispatch] = useReducer(reducer, {cat: '', dog: ''})
-    const formHandler = (e) => {
-        const data = {...state, [e.target.name]: e.target.valueO}
-    }
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+
     return (
         <div>
-            <form>
+            <form onSubmit={(e) => alert(
+                JSON.stringify({
+                    cat: state.cat,
+                    dog: state.dog
+                })
+            )}>
                 <label>Add cat:
-                    <input type="text" name={'cat'} value={state.cat} onChange={formHandler}/>
-                    <button onClick={()=>dispatch({type:'cat'})}>Save</button>
+                    <input
+                        type="text"
+                        name={'cat'}
+                        value={state.cat}
+                        onChange={(e) =>
+                            dispatch({
+                                type:'update_input',
+                                value:e.target.value,
+                                key:'cat',
+                            })
+                        }
+                    />
+                    <button>Save</button>
                 </label>
                 <label>Add dog:
-                    <input type="text" name={'dog'} value={state.dog} onChange={formHandler}/>
-                    <button onClick={()=>dispatch({type:'dog'})}>Save</button>
+                    <input
+                        type="text"
+                        name={'dog'}
+                        value={state.dog}
+                        onChange={(e) =>
+                            dispatch({
+                                type:'update_input',
+                                value:e.target.value,
+                                key:'dog',
+                            })
+                        }
+                    />
+                    <button>Save</button>
                 </label>
             </form>
             <hr/>
